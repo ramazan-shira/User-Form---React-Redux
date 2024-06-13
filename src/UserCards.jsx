18e7d1544a0c9b85e051eecb67820b2c56e7710b
ramazan-shira <ramazanshira1@gmail.com>
2024-06-13 12:40:48 +0200

    Form validation added

--- src/UserCards.jsx
+++ src/UserCards.jsx
@@ -2,15 +2,27 @@ import { useDispatch, useSelector } from "react-redux";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./style.css";
 import { deleteUser } from "./userSlice";
+import { useState } from "react";
 
 const UserCards = () => {
   const users = useSelector((state) => state.users.users);
 
+  const [onEdit, setOnEdit] = useState(false);
+
   const dispatch = useDispatch();
+
+  const handleCancel = () => {
+    setOnEdit(false);
+  };
+
+  const handleEdit = () => {
+    setOnEdit(!onEdit);
+  };
 
   const handleDelete = (id) => {
     dispatch(deleteUser({ id }));
   };
+
   return (
     <div className="user-cards">
       {users.map((user) => (
@@ -21,12 +33,25 @@ const UserCards = () => {
           <p>Email: {user.email}</p>
           <p>Area of expertise: {user.expertise}</p>
           <div className="btn">
-            <button>
-              <i class="fa-solid fa-pen-to-square"></i>
-            </button>
-            <button onClick={() => handleDelete(user.id)}>
-              <i class="fa-solid fa-trash"></i>
-            </button>
+            {onEdit ? (
+              <>
+                <button>
+                  <i className="fa-solid fa-square-check"></i>
+                </button>
+                <button onClick={handleCancel}>
+                  <i className="fa-regular fa-rectangle-xmark"></i>
+                </button>
+              </>
+            ) : (
+              <>
+                <button onClick={handleEdit}>
+                  <i className="fa-solid fa-pen-to-square"></i>
+                </button>
+                <button onClick={() => handleDelete(user.id)}>
+                  <i className="fa-solid fa-trash"></i>
+                </button>{" "}
+              </>
+            )}
           </div>
         </div>
       ))}
--- src/UserForm.jsx
+++ src/UserForm.jsx
@@ -11,15 +11,80 @@ const UserForm = () => {
   const [email, setEmail] = useState("");
   const [expertise, setExpertise] = useState("");
 
+  const [nameError, setNameError] = useState("");
+  const [ageError, setAgeError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [expertiseError, setExpertiseError] = useState("");
+
   const dispatch = useDispatch();
 
+  const [valid, setValid] = useState(false);
+
+  const fullNameLengthRegex = /^.{3,}\s.{3,}$/;
+  const fullNameCharacterRegex = /^[a-zA-Z]+\s[a-zA-Z]+$/;
+  const regEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  var ageRegex = /^(1[89]|[2-9]\d|100)$/;
+
   const handleSave = () => {
-    dispatch(addUser({ image, fullname, age, email, expertise }));
-    setImage("");
-    setFullName("");
-    setAge("");
-    setEmail("");
-    setExpertise("");
+    if (image === "") {
+      setImage(
+        "https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg"
+      );
+      setValid(true);
+    }
+
+    if (fullname === "") {
+      setNameError("Full name cannot be empty!");
+      setValid(false);
+    } else if (!fullNameLengthRegex.test(fullname)) {
+      setNameError("First name and last name must have at least 3 characters!");
+      setValid(false);
+    } else if (!fullNameCharacterRegex.test(fullname)) {
+      setNameError("First name and last name can contain only letters!");
+      setValid(false);
+    } else {
+      setNameError("");
+      setValid(true);
+    }
+
+    if (age === "") {
+      setAgeError("Age cannot be empty!");
+      setValid(false);
+    } else if (!ageRegex.test(age)) {
+      setAgeError("Age must be a number between 18 and 100 !");
+      setValid(false);
+    } else {
+      setAgeError("");
+      setValid(true);
+    }
+
+    if (email === "") {
+      setEmailError("Email cannot be empty!");
+      setValid(false);
+    } else if (!regEmail.test(email)) {
+      setEmailError("Invalid email address!");
+      setValid(false);
+    } else {
+      setValid(true);
+      setEmailError("");
+    }
+
+    if (expertise === "select") {
+      setExpertiseError("Please select an area of expertise!");
+      setValid(false);
+    } else {
+      setExpertiseError("");
+      setValid(true);
+    }
+
+    if (valid) {
+      dispatch(addUser({ image, fullname, age, email, expertise }));
+      setImage("");
+      setFullName("");
+      setAge("");
+      setEmail("");
+      setExpertise("");
+    }
   };
 
   return (
@@ -42,6 +107,7 @@ const UserForm = () => {
             value={fullname}
             onChange={(e) => setFullName(e.target.value)}
           />
+          <span className="error">{nameError}</span>
         </div>
 
         <div className="input">
@@ -52,6 +118,7 @@ const UserForm = () => {
             value={age}
             onChange={(e) => setAge(e.target.value)}
           />
+          <span className="error">{ageError}</span>
         </div>
 
         <div className="input">
@@ -62,6 +129,7 @@ const UserForm = () => {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          <span className="error">{emailError}</span>
         </div>
 
         <div className="input">
@@ -70,11 +138,13 @@ const UserForm = () => {
             value={expertise}
             onChange={(e) => setExpertise(e.target.value)}
           >
+            <option value="select">Choose one</option>
             <option value="Software Development">Software Development</option>
             <option value="Accounting">Accounting</option>
             <option value="Engineering">Engineering</option>
             <option value="Project Management">Project Management</option>
           </select>
+          <span className="error">{expertiseError}</span>
         </div>
       </div>
       <div className="save">
